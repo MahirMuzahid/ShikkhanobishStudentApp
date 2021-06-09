2d5a1af3a675be58492d51cea913b3d5514b1734
Mahir <mahirmuzahid@gmail.com>
2021-06-09 16:19:19 +0600

    Register 80% done

--- ShikkhanobishStudentApp/ShikkhanobishStudentApp/ViewModel/RegisterViewModel.cs
+++ ShikkhanobishStudentApp/ShikkhanobishStudentApp/ViewModel/RegisterViewModel.cs
@@ -3,6 +3,7 @@ using ShikkhanobishStudentApp.View;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -22,6 +23,7 @@ namespace ShikkhanobishStudentApp.ViewModel
             egTxt = "e.g: 01xxxxxxxxx";
             sendAgainVisibility = false;
             infoVisibility = false;
+            registerbtvEnabled = true;
         }
         public ICommand btnCommand =>
              new Command(() =>
@@ -35,6 +37,7 @@ namespace ShikkhanobishStudentApp.ViewModel
                      btnTxt = "Varify";
                      sendAgainColor = "gray";
                      sendAgainVisibility = true;
+                     entryText = "";
                  }
                  else if(btnNav == 2)
                  {
@@ -47,7 +50,80 @@ namespace ShikkhanobishStudentApp.ViewModel
                  }
                  else if(btnNav == 3)
                  {
-                     Application.Current.MainPage.Navigation.PushAsync(new TakeTuitionView(false));
+                     bool allok = true;
+                     if(password != null)
+                     {
+                         if (!password.Any(char.IsDigit) && password.Length < 6)
+                         {
+                             passimgsrc = "wrong.png";
+                             pasMargin = 9;
+                             allok = false;
+                         }
+                         else
+                         {
+                             passimgsrc = "right.png";
+                             pasMargin = 10;
+                         }
+                     }
+                     else
+                     {
+                         passimgsrc = "wrong.png";
+                         pasMargin = 9;
+                         allok = false;
+                     }
+                     
+                     if(conPassword != null)
+                     {
+                         if (conPassword != password)
+                         {
+                             conpassimgsrc = "wrong.png";
+                             conpasMargin = 9;
+                             allok = false;
+                         }
+                         else
+                         {
+                             conpassimgsrc = "right.png";
+                             conpasMargin = 10;
+                         }
+                     }
+                     else
+                     {
+                         conpassimgsrc = "wrong.png";
+                         conpasMargin = 9;
+                         allok = false;
+                     }
+                     
+
+                     if(name == null || name == "")
+                     {
+                         nameimgsrc = "wrong.png";
+                         nameMargin = 9;
+                         allok = false;
+                     }
+                     else
+                     {
+                         nameimgsrc = "right.png";
+                         nameMargin = 10;
+                     }
+
+                     if (city == null || city == "")
+                     {
+                         cityimgsrc = "wrong.png";
+                         cityMargin = 9;
+                         allok = false;
+                     }
+                     else
+                     {
+                         cityimgsrc = "right.png";
+                         cityMargin = 10;
+                     }
+
+                     if(allok == true)
+                     {
+                         registerbtvEnabled = true;
+                         Application.Current.MainPage.Navigation.PushAsync(new TakeTuitionView(false));
+                     }
+                     
                  }
              });
          public ICommand goBack =>
@@ -84,9 +160,65 @@ namespace ShikkhanobishStudentApp.ViewModel
 
         public string sendAgainColor { get => sendAgainColor1; set => SetProperty(ref sendAgainColor1, value); }
 
+        private string entryText1;
+
+        public string entryText { get => entryText1; set => SetProperty(ref entryText1, value); }
+
         private bool infoVisibility1;
 
         public bool infoVisibility { get => infoVisibility1; set => SetProperty(ref infoVisibility1, value); }
+
+        private string password1;
+
+        public string password { get => password1; set => SetProperty(ref password1, value); }
+
+        private string conPassword1;
+
+        public string conPassword { get => conPassword1; set => SetProperty(ref conPassword1, value); }
+
+        private string name1;
+
+        public string name { get => name1; set => SetProperty(ref name1, value); }
+
+        private string city1;
+
+        public string city { get => city1; set => SetProperty(ref city1, value); }
+
+        private string passimgsrc1;
+
+        public string passimgsrc { get => passimgsrc1; set => SetProperty(ref passimgsrc1, value); }
+
+        private Thickness nameMargin1;
+
+        public Thickness nameMargin { get => nameMargin1; set => SetProperty(ref nameMargin1, value); }
+
+        private string conpassimgsrc1;
+
+        public string conpassimgsrc { get => conpassimgsrc1; set => SetProperty(ref conpassimgsrc1, value); }
+
+        private Thickness pasMargin1;
+
+        public Thickness pasMargin { get => pasMargin1; set => SetProperty(ref pasMargin1, value); }
+
+        private string nameimgsrc1;
+
+        public string nameimgsrc { get => nameimgsrc1; set => SetProperty(ref nameimgsrc1, value); }
+
+        private Thickness cityMargin1;
+
+        public Thickness cityMargin { get => cityMargin1; set => SetProperty(ref cityMargin1, value); }
+
+        private string cityimgsrc1;
+
+        public string cityimgsrc { get => cityimgsrc1; set => SetProperty(ref cityimgsrc1, value); }
+
+        private Thickness conpasMargin1;
+
+        public Thickness conpasMargin { get => conpasMargin1; set => SetProperty(ref conpasMargin1, value); }
+
+        private bool registerbtvEnabled1;
+
+        public bool registerbtvEnabled { get => registerbtvEnabled1; set => SetProperty(ref registerbtvEnabled1, value); }
         #endregion
 
     }
