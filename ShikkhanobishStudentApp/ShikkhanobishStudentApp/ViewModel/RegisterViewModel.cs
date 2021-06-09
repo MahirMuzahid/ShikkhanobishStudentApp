6cfa09cbe9e06c3f73c1dadfdef873896e62b89d
Mahir <mahirmuzahid@gmail.com>
2021-06-09 17:57:30 +0600

    back logic done

--- ShikkhanobishStudentApp/ShikkhanobishStudentApp/ViewModel/RegisterViewModel.cs
+++ ShikkhanobishStudentApp/ShikkhanobishStudentApp/ViewModel/RegisterViewModel.cs
@@ -24,107 +24,154 @@ namespace ShikkhanobishStudentApp.ViewModel
             sendAgainVisibility = false;
             infoVisibility = false;
             registerbtvEnabled = true;
+            otpVisibility = false;
         }
         public ICommand btnCommand =>
-             new Command(() =>
+             new Command<string>((index) =>
              {
-                 if(btnNav == 1)
+                 if(int.Parse(index) == 0)
                  {
-                     btnNav++;
-                     titleTxt = "Enter 5 Digit OTP ";
-                     numberVisibility = false;
-                     egTxt = "";
-                     btnTxt = "Varify";
-                     sendAgainColor = "gray";
-                     sendAgainVisibility = true;
-                     entryText = "";
-                 }
-                 else if(btnNav == 2)
-                 {
-                     btnNav++;
-                     varificationvisibility = false;
-                     titleTxt = "Enter Your Information";
-                     infoVisibility = true;
-                     btnTxt = "Register";
+                     btnNav = 1;
+                     btnTxt = "Send Otp";
+                     varificationvisibility = true;
+                     numberVisibility = true;
+                     titleTxt = "Enter Your Phone Number";
+                     egTxt = "e.g: 01xxxxxxxxx";
                      sendAgainVisibility = false;
+                     infoVisibility = false;
+                     registerbtvEnabled = true;
+                     otpfiText = "";
+                     otpsecText = "";
+                     otpthText = "";
+                     otpfrText = "";
+                     otpfivText = "";
                  }
-                 else if(btnNav == 3)
+                 else
                  {
-                     bool allok = true;
-                     if(password != null)
+                     if (btnNav == 1)
                      {
-                         if (!password.Any(char.IsDigit) && password.Length < 6)
+                         btnNav++;
+                         titleTxt = "Enter 5 Digit OTP ";
+                         numberVisibility = false;
+                         egTxt = "";
+                         btnTxt = "Varify";
+                         sendAgainColor = "gray";
+                         sendAgainVisibility = true;
+                         varificationvisibility = false;
+                         otpVisibility = true;
+                         entryText = "";
+                     }
+                     else if (btnNav == 2)
+                     {
+                         //send otp to mobile
+                         string otp = "12345";
+                         if (otpfiText != null && otpsecText != null && otpthText != null && otpfrText != null && otpfivText != null)
+                         {
+                             if(otpfiText == otp[0].ToString() && otpsecText == otp[1].ToString() && otpthText == otp[2].ToString() && otpfrText == otp[3].ToString() && otpfivText == otp[4].ToString() )
+                             {
+                                 btnNav++;
+                                 errorText = "";
+                                 otpVisibility = false;
+                                 titleTxt = "Enter Your Information";
+                                 infoVisibility = true;
+                                 btnTxt = "Register";
+                                 sendAgainVisibility = false;
+                             }
+                             else
+                             {
+                                 errorText = "OTP doesnt match";
+                             }
+                             
+                         }
+                         else
+                         {
+                             errorText = "OTP can't be empty";
+                         }
+
+                     }
+                     else if (btnNav == 3)
+                     {
+                         bool allok = true;
+                         if (password != null)
+                         {
+                             if (!password.Any(char.IsDigit) && password.Length < 6)
+                             {
+                                 passimgsrc = "wrong.png";
+                                 pasMargin = 9;
+                                 allok = false;
+                                 errorText = "Password length should be 6 and at least contain one digit";
+                             }
+                             else
+                             {
+                                 passimgsrc = "right.png";
+                                 pasMargin = 10;
+                             }
+                         }
+                         else
                          {
                              passimgsrc = "wrong.png";
                              pasMargin = 9;
                              allok = false;
                          }
-                         else
+
+                         if (conPassword != null)
                          {
-                             passimgsrc = "right.png";
-                             pasMargin = 10;
+                             if (conPassword != password)
+                             {
+                                 conpassimgsrc = "wrong.png";
+                                 conpasMargin = 9;
+                                 allok = false;
+                                 errorText = "Password doesn't match";
+                             }
+                             else
+                             {
+                                 conpassimgsrc = "right.png";
+                                 conpasMargin = 10;
+                             }
                          }
-                     }
-                     else
-                     {
-                         passimgsrc = "wrong.png";
-                         pasMargin = 9;
-                         allok = false;
-                     }
-                     
-                     if(conPassword != null)
-                     {
-                         if (conPassword != password)
+                         else
                          {
                              conpassimgsrc = "wrong.png";
                              conpasMargin = 9;
                              allok = false;
                          }
+
+
+                         if (name == null || name == "")
+                         {
+                             nameimgsrc = "wrong.png";
+                             nameMargin = 9;
+                             allok = false;
+                             errorText = "Name shouldn't be empty";
+                         }
                          else
                          {
-                             conpassimgsrc = "right.png";
-                             conpasMargin = 10;
+                             nameimgsrc = "right.png";
+                             nameMargin = 10;
                          }
-                     }
-                     else
-                     {
-                         conpassimgsrc = "wrong.png";
-                         conpasMargin = 9;
-                         allok = false;
-                     }
-                     
 
-                     if(name == null || name == "")
-                     {
-                         nameimgsrc = "wrong.png";
-                         nameMargin = 9;
-                         allok = false;
-                     }
-                     else
-                     {
-                         nameimgsrc = "right.png";
-                         nameMargin = 10;
-                     }
+                         if (city == null || city == "")
+                         {
+                             cityimgsrc = "wrong.png";
+                             cityMargin = 9;
+                             allok = false;
+                             errorText = "City shouldn't be empty";
+                         }
+                         else
+                         {
+                             cityimgsrc = "right.png";
+                             cityMargin = 10;
+                         }
 
-                     if (city == null || city == "")
-                     {
-                         cityimgsrc = "wrong.png";
-                         cityMargin = 9;
-                         allok = false;
-                     }
-                     else
-                     {
-                         cityimgsrc = "right.png";
-                         cityMargin = 10;
-                     }
+                         if (allok == true)
+                         {
+                             registerbtvEnabled = true;
+                             Application.Current.MainPage.Navigation.PushAsync(new TakeTuitionView(false));
+                         }
 
-                     if(allok == true)
-                     {
-                         registerbtvEnabled = true;
-                         Application.Current.MainPage.Navigation.PushAsync(new TakeTuitionView(false));
                      }
-                     
                  }
+                 
              });
          public ICommand goBack =>
              new Command(() =>
@@ -219,6 +266,34 @@ namespace ShikkhanobishStudentApp.ViewModel
         private bool registerbtvEnabled1;
 
         public bool registerbtvEnabled { get => registerbtvEnabled1; set => SetProperty(ref registerbtvEnabled1, value); }
+
+        private bool otpVisibility1;
+
+        public bool otpVisibility { get => otpVisibility1; set => SetProperty(ref otpVisibility1, value); }
+
+        private string otpfiText1;
+
+        public string otpfiText { get => otpfiText1; set => SetProperty(ref otpfiText1, value); }
+
+        private string otpsecText1;
+
+        public string otpsecText { get => otpsecText1; set => SetProperty(ref otpsecText1, value); }
+
+        private string otpfrText1;
+
+        public string otpfrText { get => otpfrText1; set => SetProperty(ref otpfrText1, value); }
+
+        private string otpthText1;
+
+        public string otpthText { get => otpthText1; set => SetProperty(ref otpthText1, value); }
+
+        private string errorText1;
+
+        public string errorText { get => errorText1; set => SetProperty(ref errorText1, value); }
+
+        private string otpfivText1;
+
+        public string otpfivText { get => otpfivText1; set => SetProperty(ref otpfivText1, value); }
         #endregion
 
     }
