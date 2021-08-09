4ebba372faf05810036dc1f85efe965d7fd9f45b
Mahir Muzahid <mahirmuzahid@gmail.com>
2021-08-09 22:08:29 +0600

    new logo

--- ShikkhanobishStudentApp/ShikkhanobishStudentApp.Android/Resources/Resource.designer.cs
+++ ShikkhanobishStudentApp/ShikkhanobishStudentApp.Android/Resources/Resource.designer.cs
@@ -23904,58 +23904,58 @@ namespace ShikkhanobishStudentApp.Droid
 			public const int logo = 2131165319;
 			
 			// aapt resource value: 0x7F070088
-			public const int material_ic_calendar_black_24dp = 2131165320;
+			public const int logowhite = 2131165320;
 			
 			// aapt resource value: 0x7F070089
-			public const int material_ic_clear_black_24dp = 2131165321;
+			public const int material_ic_calendar_black_24dp = 2131165321;
 			
 			// aapt resource value: 0x7F07008A
-			public const int material_ic_edit_black_24dp = 2131165322;
+			public const int material_ic_clear_black_24dp = 2131165322;
 			
 			// aapt resource value: 0x7F07008B
-			public const int material_ic_keyboard_arrow_left_black_24dp = 2131165323;
+			public const int material_ic_edit_black_24dp = 2131165323;
 			
 			// aapt resource value: 0x7F07008C
-			public const int material_ic_keyboard_arrow_right_black_24dp = 2131165324;
+			public const int material_ic_keyboard_arrow_left_black_24dp = 2131165324;
 			
 			// aapt resource value: 0x7F07008D
-			public const int material_ic_menu_arrow_down_black_24dp = 2131165325;
+			public const int material_ic_keyboard_arrow_right_black_24dp = 2131165325;
 			
 			// aapt resource value: 0x7F07008E
-			public const int material_ic_menu_arrow_up_black_24dp = 2131165326;
+			public const int material_ic_menu_arrow_down_black_24dp = 2131165326;
 			
 			// aapt resource value: 0x7F07008F
-			public const int mtrl_dialog_background = 2131165327;
+			public const int material_ic_menu_arrow_up_black_24dp = 2131165327;
 			
 			// aapt resource value: 0x7F070090
-			public const int mtrl_dropdown_arrow = 2131165328;
+			public const int mtrl_dialog_background = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int mtrl_ic_arrow_drop_down = 2131165329;
+			public const int mtrl_dropdown_arrow = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int mtrl_ic_arrow_drop_up = 2131165330;
+			public const int mtrl_ic_arrow_drop_down = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int mtrl_ic_cancel = 2131165331;
+			public const int mtrl_ic_arrow_drop_up = 2131165331;
 			
 			// aapt resource value: 0x7F070094
-			public const int mtrl_ic_error = 2131165332;
+			public const int mtrl_ic_cancel = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int mtrl_popupmenu_background = 2131165333;
+			public const int mtrl_ic_error = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int mtrl_popupmenu_background_dark = 2131165334;
+			public const int mtrl_popupmenu_background = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int mtrl_tabs_default_indicator = 2131165335;
+			public const int mtrl_popupmenu_background_dark = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int navigation_empty_icon = 2131165336;
+			public const int mtrl_tabs_default_indicator = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int newLogo = 2131165337;
+			public const int navigation_empty_icon = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
 			public const int newlogowhite = 2131165338;
